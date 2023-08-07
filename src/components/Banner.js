0965e360f8c49a184e79bf72fe5fab712ae3869f
maggiemarks1 <maggiemarkus1@gmail.com>
2023-08-07 16:15:46 -0500

    completed project

---
+++ src/components/Banner.js
@@ -0,0 +1,10 @@
+import styling from "./styling.css"
+const Banner = () => {
+    return(
+ <>
+ <h1>Orbit Report</h1>
+    <p>Click on the buttons to see the satellites in that orbit type</p>
+    </>
+    );
+};
+export default Banner;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,18 +1,28 @@
+
+import styling from "./styling.css"
 const Table = ({ sat }) => {
   return (
       <table>
        <thead>
         <tr>
-          <th>Header TBD</th>
+          <th> Name</th>
+          <th> Type of Satellite</th>
+          <th> Launch Date</th>
+          <th> Status</th>
         </tr>
         </thead>
         <tbody>
-        <tr>
-          <td>Row Data TBD</td>
-        </tr>
+          {sat.map((data)=>{
+            return(
+        <tr key = {data.id}>
+          <td>{data.name}</td>
+          <td>{data.type}</td>
+          <td>{data.launchDate}</td>
+          <td>{data.operational === true ? "Active" : "Inactive"}</td>
+        </tr>)
+        })}
         </tbody>
       </table>
   );
 };
-
 export default Table;
